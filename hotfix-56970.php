ebf8f26b7e7ee1f614afea5954fd4d0a72bb2461
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-12-01 16:50:45 -0800

    Add theme.json cache hotfixes

--- hotfix-56970.php
+++ hotfix-56970.php
@@ -19,6 +19,29 @@ function wp_hotfix_56970_init() {
 
     // Issue described in Trac 56970 only affects upgrade from < 6.1 to 6.1.1.
     if ( '6.1.1' === $wp_version ) {
+        // Unhook default cache cleaning mechanism.
+        remove_action( 'switch_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+        remove_action( 'start_previewing_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+
+        // Hook override cache cleaning mechanism.
+        add_action( 'switch_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
+        add_action( 'start_previewing_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
+        add_action( 'plugins_loaded', 'wp_hotfix_611_wp_add_non_persistent_theme_json_cache_group' );
     }
 }
 
+/**
+ * Clean the caches under the theme_json group.
+ */
+function wp_hotfix_611_wp_clean_theme_json_caches() {
+    wp_cache_delete( 'wp_get_global_stylesheet', 'theme_json' );
+    WP_Theme_JSON_Resolver::clean_cached_data();
+}
+
+/**
+ * Tell cache mechanisms not to persist theme.json data across requests for data
+ * stored under the wp_get_global_stylesheet cache group.
+ */
+function wp_hotfix_611_wp_add_non_persistent_theme_json_cache_group() {
+    wp_cache_add_non_persistent_groups( 'theme_json' );
+}
