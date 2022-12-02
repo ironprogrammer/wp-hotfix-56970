a8186bb07f8346fcf7b7c100dece7c66b49697c7
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-12-01 16:41:25 -0800

    Stub plugin

---
+++ hotfix-56970.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Hotfix for Trac 56970
+ * Description: Cleans theme.json cache. See <a href="https://core.trac.wordpress.org/ticket/56970">Trac 56970</a>.
+ * Version: 0.1
+ */
+
