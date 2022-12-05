13049cf0e6403e1dcde7c61302f81f3df984e1f4
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-12-05 14:05:03 -0800

    Fix spaces to tabs formatting

--- hotfix-56970.php
+++ hotfix-56970.php
@@ -9,36 +9,36 @@
 WP_Hotfix_56970_Controller::init();
 
 class WP_Hotfix_56970_Controller {
-    public static function init() {
-        add_action( 'init', 'wp_hotfix_56970_init' );
-    }
+	public static function init() {
+		add_action( 'init', 'wp_hotfix_56970_init' );
+	}
 }
 
 function wp_hotfix_56970_init() {
-    global $wp_version;
+	global $wp_version;
 
-    // Issue described in Trac 56970 only affects upgrade from < 6.1 to 6.1.1.
-    if ( '6.1.1' === $wp_version ) {
-        // Unhook default global styles enqueue.
-        remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
-        remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+	// Issue described in Trac 56970 only affects upgrade from < 6.1 to 6.1.1.
+	if ( '6.1.1' === $wp_version ) {
+		// Unhook default global styles enqueue.
+		remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+		remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
 
-        // Hook override global styles enqueue.
-        add_action( 'wp_enqueue_scripts', 'wp_hotfix_611_enqueue_global_styles' );
-        add_action( 'wp_footer', 'wp_hotfix_611_enqueue_global_styles', 1 );
+		// Hook override global styles enqueue.
+		add_action( 'wp_enqueue_scripts', 'wp_hotfix_611_enqueue_global_styles' );
+		add_action( 'wp_footer', 'wp_hotfix_611_enqueue_global_styles', 1 );
 
-        // Hook override for block editor styles.
-        add_filter( 'block_editor_settings_all', 'wp_hotfix_611_get_block_editor_settings', PHP_INT_MAX );
+		// Hook override for block editor styles.
+		add_filter( 'block_editor_settings_all', 'wp_hotfix_611_get_block_editor_settings', PHP_INT_MAX );
 
-        // Unhook default cache cleaning mechanism.
-        remove_action( 'switch_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
-        remove_action( 'start_previewing_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+		// Unhook default cache cleaning mechanism.
+		remove_action( 'switch_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+		remove_action( 'start_previewing_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
 
-        // Hook override cache cleaning mechanism.
-        add_action( 'switch_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
-        add_action( 'start_previewing_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
-        add_action( 'plugins_loaded', 'wp_hotfix_611_wp_add_non_persistent_theme_json_cache_group' );
-    }
+		// Hook override cache cleaning mechanism.
+		add_action( 'switch_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
+		add_action( 'start_previewing_theme', 'wp_hotfix_611_wp_clean_theme_json_caches' );
+		add_action( 'plugins_loaded', 'wp_hotfix_611_wp_add_non_persistent_theme_json_cache_group' );
+	}
 }
 
 /**
@@ -47,43 +47,43 @@ function wp_hotfix_56970_init() {
  * Enqueues the global styles defined via theme.json.
  */
 function wp_hotfix_611_enqueue_global_styles() {
-    $separate_assets  = wp_should_load_separate_core_block_assets();
-    $is_block_theme   = wp_is_block_theme();
-    $is_classic_theme = ! $is_block_theme;
+	$separate_assets  = wp_should_load_separate_core_block_assets();
+	$is_block_theme   = wp_is_block_theme();
+	$is_classic_theme = ! $is_block_theme;
 
-    /*
-     * Global styles should be printed in the head when loading all styles combined.
-     * The footer should only be used to print global styles for classic themes with separate core assets enabled.
-     *
-     * See https://core.trac.wordpress.org/ticket/53494.
-     */
-    if (
-        ( $is_block_theme && doing_action( 'wp_footer' ) ) ||
-        ( $is_classic_theme && doing_action( 'wp_footer' ) && ! $separate_assets ) ||
-        ( $is_classic_theme && doing_action( 'wp_enqueue_scripts' ) && $separate_assets )
-    ) {
-        return;
-    }
+	/*
+	 * Global styles should be printed in the head when loading all styles combined.
+	 * The footer should only be used to print global styles for classic themes with separate core assets enabled.
+	 *
+	 * See https://core.trac.wordpress.org/ticket/53494.
+	 */
+	if (
+		( $is_block_theme && doing_action( 'wp_footer' ) ) ||
+		( $is_classic_theme && doing_action( 'wp_footer' ) && ! $separate_assets ) ||
+		( $is_classic_theme && doing_action( 'wp_enqueue_scripts' ) && $separate_assets )
+	) {
+		return;
+	}
 
-    /*
-     * If loading the CSS for each block separately, then load the theme.json CSS conditionally.
-     * This removes the CSS from the global-styles stylesheet and adds it to the inline CSS for each block.
-     * This filter must be registered before calling wp_get_global_stylesheet();
-     */
-    add_filter( 'wp_theme_json_get_style_nodes', 'wp_filter_out_block_nodes' );
+	/*
+	 * If loading the CSS for each block separately, then load the theme.json CSS conditionally.
+	 * This removes the CSS from the global-styles stylesheet and adds it to the inline CSS for each block.
+	 * This filter must be registered before calling wp_get_global_stylesheet();
+	 */
+	add_filter( 'wp_theme_json_get_style_nodes', 'wp_filter_out_block_nodes' );
 
-    $stylesheet = wp_hotfix_611_get_global_stylesheet();
+	$stylesheet = wp_hotfix_611_get_global_stylesheet();
 
-    if ( empty( $stylesheet ) ) {
-        return;
-    }
+	if ( empty( $stylesheet ) ) {
+		return;
+	}
 
-    wp_register_style( 'global-styles', false, array(), true, true );
-    wp_add_inline_style( 'global-styles', $stylesheet );
-    wp_enqueue_style( 'global-styles' );
+	wp_register_style( 'global-styles', false, array(), true, true );
+	wp_add_inline_style( 'global-styles', $stylesheet );
+	wp_enqueue_style( 'global-styles' );
 
-    // Add each block as an inline css.
-    wp_add_global_styles_for_blocks();
+	// Add each block as an inline css.
+	wp_add_global_styles_for_blocks();
 }
 
 /**
@@ -98,68 +98,68 @@ function wp_hotfix_611_enqueue_global_styles() {
  * @return string Stylesheet.
  */
 function wp_hotfix_611_get_global_stylesheet( $types = array() ) {
-    // Ignore cache when `WP_DEBUG` is enabled, so it doesn't interfere with the theme developers workflow.
-    $can_use_cached = empty( $types ) && ! WP_DEBUG;
-    $cache_key      = 'wp_get_global_stylesheet';
-    $cache_group    = 'theme_json';
-    if ( $can_use_cached ) {
-        $cached = wp_cache_get( $cache_key, $cache_group );
-        if ( $cached ) {
-            return $cached;
-        }
-    }
-    $tree                = WP_Theme_JSON_Resolver::get_merged_data();
-    $supports_theme_json = WP_Theme_JSON_Resolver::theme_has_support();
-    if ( empty( $types ) && ! $supports_theme_json ) {
-        $types = array( 'variables', 'presets', 'base-layout-styles' );
-    } elseif ( empty( $types ) ) {
-        $types = array( 'variables', 'styles', 'presets' );
-    }
+	// Ignore cache when `WP_DEBUG` is enabled, so it doesn't interfere with the theme developers workflow.
+	$can_use_cached = empty( $types ) && ! WP_DEBUG;
+	$cache_key      = 'wp_get_global_stylesheet';
+	$cache_group    = 'theme_json';
+	if ( $can_use_cached ) {
+		$cached = wp_cache_get( $cache_key, $cache_group );
+		if ( $cached ) {
+			return $cached;
+		}
+	}
+	$tree                = WP_Theme_JSON_Resolver::get_merged_data();
+	$supports_theme_json = WP_Theme_JSON_Resolver::theme_has_support();
+	if ( empty( $types ) && ! $supports_theme_json ) {
+		$types = array( 'variables', 'presets', 'base-layout-styles' );
+	} elseif ( empty( $types ) ) {
+		$types = array( 'variables', 'styles', 'presets' );
+	}
 
-    /*
-     * If variables are part of the stylesheet, then add them.
-     * This is so themes without a theme.json still work as before 5.9:
-     * they can override the default presets.
-     * See https://core.trac.wordpress.org/ticket/54782
-     */
-    $styles_variables = '';
-    if ( in_array( 'variables', $types, true ) ) {
-        /*
-         * Only use the default, theme, and custom origins. Why?
-         * Because styles for `blocks` origin are added at a later phase
-         * (i.e. in the render cycle). Here, only the ones in use are rendered.
-         * @see wp_add_global_styles_for_blocks
-         */
-        $origins          = array( 'default', 'theme', 'custom' );
-        $styles_variables = $tree->get_stylesheet( array( 'variables' ), $origins );
-        $types            = array_diff( $types, array( 'variables' ) );
-    }
+	/*
+	 * If variables are part of the stylesheet, then add them.
+	 * This is so themes without a theme.json still work as before 5.9:
+	 * they can override the default presets.
+	 * See https://core.trac.wordpress.org/ticket/54782
+	 */
+	$styles_variables = '';
+	if ( in_array( 'variables', $types, true ) ) {
+		/*
+		 * Only use the default, theme, and custom origins. Why?
+		 * Because styles for `blocks` origin are added at a later phase
+		 * (i.e. in the render cycle). Here, only the ones in use are rendered.
+		 * @see wp_add_global_styles_for_blocks
+		 */
+		$origins          = array( 'default', 'theme', 'custom' );
+		$styles_variables = $tree->get_stylesheet( array( 'variables' ), $origins );
+		$types            = array_diff( $types, array( 'variables' ) );
+	}
 
-    /*
-     * For the remaining types (presets, styles), we do consider origins:
-     *
-     * - themes without theme.json: only the classes for the presets defined by core
-     * - themes with theme.json: the presets and styles classes, both from core and the theme
-     */
-    $styles_rest = '';
-    if ( ! empty( $types ) ) {
-        /*
-         * Only use the default, theme, and custom origins. Why?
-         * Because styles for `blocks` origin are added at a later phase
-         * (i.e. in the render cycle). Here, only the ones in use are rendered.
-         * @see wp_add_global_styles_for_blocks
-         */
-        $origins = array( 'default', 'theme', 'custom' );
-        if ( ! $supports_theme_json ) {
-            $origins = array( 'default' );
-        }
-        $styles_rest = $tree->get_stylesheet( $types, $origins );
-    }
-    $stylesheet = $styles_variables . $styles_rest;
-    if ( $can_use_cached ) {
-        wp_cache_set( $cache_key, $stylesheet, $cache_group );
-    }
-    return $stylesheet;
+	/*
+	 * For the remaining types (presets, styles), we do consider origins:
+	 *
+	 * - themes without theme.json: only the classes for the presets defined by core
+	 * - themes with theme.json: the presets and styles classes, both from core and the theme
+	 */
+	$styles_rest = '';
+	if ( ! empty( $types ) ) {
+		/*
+		 * Only use the default, theme, and custom origins. Why?
+		 * Because styles for `blocks` origin are added at a later phase
+		 * (i.e. in the render cycle). Here, only the ones in use are rendered.
+		 * @see wp_add_global_styles_for_blocks
+		 */
+		$origins = array( 'default', 'theme', 'custom' );
+		if ( ! $supports_theme_json ) {
+			$origins = array( 'default' );
+		}
+		$styles_rest = $tree->get_stylesheet( $types, $origins );
+	}
+	$stylesheet = $styles_variables . $styles_rest;
+	if ( $can_use_cached ) {
+		wp_cache_set( $cache_key, $stylesheet, $cache_group );
+	}
+	return $stylesheet;
 }
 
 /**
@@ -220,8 +220,8 @@ function wp_hotfix_611_get_block_editor_settings( $settings ) {
  * Clean the caches under the theme_json group.
  */
 function wp_hotfix_611_wp_clean_theme_json_caches() {
-    wp_cache_delete( 'wp_get_global_stylesheet', 'theme_json' );
-    WP_Theme_JSON_Resolver::clean_cached_data();
+	wp_cache_delete( 'wp_get_global_stylesheet', 'theme_json' );
+	WP_Theme_JSON_Resolver::clean_cached_data();
 }
 
 /**
@@ -229,5 +229,5 @@ function wp_hotfix_611_wp_clean_theme_json_caches() {
  * stored under the wp_get_global_stylesheet cache group.
  */
 function wp_hotfix_611_wp_add_non_persistent_theme_json_cache_group() {
-    wp_cache_add_non_persistent_groups( 'theme_json' );
+	wp_cache_add_non_persistent_groups( 'theme_json' );
 }
